11ee84022e6d3c15339e9bb7dca4137cb8f74b39
deblur99 <deblur99@kakao.com>
2021-11-08 23:51:33 +0900

    added-11-8

--- ConsoleApp1/DewPointCalculator.cs
+++ ConsoleApp1/DewPointCalculator.cs
@@ -4,19 +4,113 @@ namespace ConsoleApp1
 {
     class DewPointCalculator
     {
-        // double 매개변수 2개를 갖는 메서드 선언 및 정의
-        public static double CalculateDewPoint(double RH, double T)
+        private double temperature; // fahrenheit
+
+        private double relativeHumidity; // %
+
+        private double value; // DewPoint 값
+
+        public double Temperature
         {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                temperature = value;
+            }
+        }
+        
+        public double RelativeHumidity
+        {
+            get
+            {
+                return relativeHumidity;
+            }
+            set
+            {
+                relativeHumidity = value;
+            }
+        }
+        
+        public double Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        DewPointCalculator()
+        {
+            Console.WriteLine("Calculate DewPoint");    
+        }
+        
+        public void Calculate()
+        {
+             value = 243.12 * (Math.Log(relativeHumidity / 100) + 17.62 * temperature / (243.12 + temperature)) /
+                    (17.62 - (Math.Log(relativeHumidity / 100) + 17.62 * temperature / (243.12 + temperature)));
+        }
+        
+        // double 매개변수 2개를 갖는 메서드 선언 및 정의
+        public static double CalculateDewPoint(double F, double RH)
+        {
+            double C = FahrenheitToCelsius(F);
             double dewPoint;
 
             // C#에서의 로그 값을 구하는 함수는 Math 라이브러리에 내장되어 있다.
-            dewPoint = 243.12 * (Math.Log(RH / 100) + 17.62 * T / (243.12 + T)) /
-                       (17.62 - (Math.Log(RH / 100) + 17.62 * T / (243.12 + T)));
+            dewPoint = 243.12 * (Math.Log(RH / 100) + 17.62 * C / (243.12 + C)) /
+                       (17.62 - (Math.Log(RH / 100) + 17.62 * C / (243.12 + C)));
 
             // 소수 둘째 자리에서 반올림하여 실수 값을 소수점 하나만 남기는 Math 라이브러리의 Round 함수 호출
-            return Math.Round(dewPoint, 1);
+            return Math.Round(CelsiusToFahrenheit(dewPoint), 1);
+        }
+        
+        // 단위 환산을 위한 메서드
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return 1.8f * c + 32;
+        }
+        
+        public static double FahrenheitToCelsius(double f)
+        {
+            return (f - 32) * 5 / 9;
+        }
+        
+        // 사용자가 입력한 
+        public override String ToString()
+        {
+            return String.Format("DewPointCalculator [Temperature={0}, RelativeHumidity={1}, Value={2}]",
+                temperature, relativeHumidity, value);
         }
 
+        public void GetUserInput()
+        {
+            double temperature, relativeHumidity;
+            String input;
+            
+            Console.WriteLine("Please enter temperature (F) >>");
+            input = Console.Read().ToString();
+            Double.TryParse(input, out temperature);
+            
+            Console.WriteLine("Please enter relative humidity (%) >>");
+            input = Console.Read().ToString();
+            Double.TryParse(input, out relativeHumidity);
+            
+            Calculate();
+        }
+        
+        // 이슬점 테이블 출력
+        public static void PrintTable()
+        {
+            
+        }
+        
         static void Main(string[] args)
         {
             // 변수 선언
