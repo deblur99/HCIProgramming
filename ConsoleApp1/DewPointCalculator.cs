e75f5c2c9c562049cdcf35a2b329ae931728d8f0
deblur99 <deblur99@kakao.com>
2021-11-09 03:40:06 +0900

    added-11-9

--- ConsoleApp1/DewPointCalculator.cs
+++ ConsoleApp1/DewPointCalculator.cs
@@ -12,91 +12,151 @@ namespace ConsoleApp1
 
         public double Temperature
         {
-            get
-            {
-                return temperature;
-            }
-            set
-            {
-                temperature = value;
-            }
+            get { return temperature; }
+            set { temperature = value; }
         }
-        
+
         public double RelativeHumidity
         {
-            get
-            {
-                return relativeHumidity;
-            }
-            set
-            {
-                relativeHumidity = value;
-            }
+            get { return relativeHumidity; }
+            set { relativeHumidity = value; }
         }
-        
+
         public double Value
         {
-            get
-            {
-                return value;
-            }
-            set
-            {
-                this.value = value;
-            }
+            get { return value; }
+            set { this.value = value; }
         }
 
         public virtual void Calculate()
         {
             return;
         }
-        
+
         public static double CelsiusToFahrenheit(double c)
         {
             return 1.8f * c + 32;
         }
-        
+
         public static double FahrenheitToCelsius(double f)
         {
             return (f - 32) * 5 / 9;
         }
-        
+
         // 사용자로부터 화씨 온도와 상대습도 입력받기
         public void GetUserInput()
         {
             String input;
-            
+
             Console.WriteLine("Please enter temperature (F) >>");
             input = Console.Read().ToString();
             Double.TryParse(input, out temperature);
-            
+
             Console.WriteLine("Please enter relative humidity (%) >>");
             input = Console.Read().ToString();
             Double.TryParse(input, out relativeHumidity);
-            
+
             Calculate();
         }
-        
+
         // 이슬점 테이블 출력
-        public static void PrintTable()
+        public virtual void PrintTable()
         {
-            
+            return;
         }
     }
-    
+
     class DewPointCalculator : Calculator
     {
-        DewPointCalculator()
+        // 테이블
+        // 존재하지 않는 값은 -1로 처리
+        private readonly string[,] _dewPointTable = new string[,]
         {
-            Console.WriteLine("Calculate DewPoint");    
+            {
+                "F/RH", "100", "95", "90", "85", "80", "75", "70", "65", "60", "55", "50", "45", "40", "35", "30", "25",
+                "20", "15", "10"
+            },
+            {
+                "110", "110", "108", "106", "104", "102", "100", "98", "95", "93", "90", "87", "84", "80", "76", "72",
+                "65", "60", "51", "41"
+            },
+            {
+                "105", "105", "103", "101", "99", "97", "95", "93", "91", "88", "85", "83", "80", "76", "72", "67",
+                "62", "55", "47", "37"
+            },
+            {
+                "100", "100", "99", "97", "95", "93", "91", "89", "86", "84", "81", "78", "72", "71", "67", "63", "58",
+                "52", "44", "32"
+            },
+            {
+                "95", "95", "93", "92", "90", "88", "86", "84", "81", "79", "76", "73", "70", "67", "63", "59", "54",
+                "48", "40", "32"
+            },
+            {
+                "90", "90", "88", "87", "85", "83", "81", "79", "76", "74", "71", "68", "65", "62", "59", "54", "49",
+                "43", "36", "32"
+            },
+            {
+                "85", "85", "83", "81", "80", "78", "76", "74", "72", "69", "67", "64", "61", "58", "54", "50", "45",
+                "38", "32", "-1"
+            },
+            {
+                "80", "80", "78", "77", "75", "73", "71", "69", "67", "65", "62", "59", "56", "53", "50", "45", "40",
+                "35", "32", "-1"
+            },
+            {
+                "75", "75", "73", "72", "70", "68", "66", "64", "62", "60", "58", "55", "52", "49", "45", "41", "36",
+                "32", "-1", "-1"
+            },
+            {
+                "70", "70", "68", "67", "65", "63", "61", "59", "57", "55", "53", "50", "47", "44", "40", "37", "32",
+                "-1", "-1", "-1"
+            },
+            {
+                "65", "65", "63", "62", "60", "59", "57", "55", "53", "50", "48", "45", "42", "40", "36", "32", "-1",
+                "-1", "-1", "-1"
+            },
+            {
+                "60", "60", "58", "57", "55", "53", "52", "50", "48", "45", "43", "41", "38", "35", "32", "-1", "-1",
+                "-1", "-1", "-1"
+            },
+            {
+                "55", "55", "53", "52", "50", "49", "47", "45", "43", "40", "38", "36", "33", "32", "-1", "-1", "-1",
+                "-1", "-1", "-1"
+            },
+            {
+                "50", "50", "48", "46", "45", "44", "42", "40", "38", "36", "34", "32", "-1", "-1", "-1", "-1", "-1",
+                "-1", "-1", "-1"
+            },
+            {
+                "45", "45", "43", "42", "40", "39", "37", "35", "33", "32", "-1", "-1", "-1", "-1", "-1", "-1", "-1",
+                "-1", "-1", "-1"
+            },
+            {
+                "40", "40", "39", "37", "35", "34", "32", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1",
+                "-1", "-1", "-1"
+            },
+            {
+                "35", "35", "34", "32", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1",
+                "-1", "-1", "-1"
+            },
+            {
+                "32", "32", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1",
+                "-1", "-1", "-1"
+            }
+        };
+
+        public DewPointCalculator()
+        {
+            Console.WriteLine("Calculate DewPoint");
         }
-        
+
         public override void Calculate()
         {
-             value = 243.12 * (Math.Log(relativeHumidity / 100) + 17.62 * temperature / (243.12 + temperature)) /
+            value = 243.12 * (Math.Log(relativeHumidity / 100) + 17.62 * temperature / (243.12 + temperature)) /
                     (17.62 - (Math.Log(relativeHumidity / 100) + 17.62 * temperature / (243.12 + temperature)));
         }
-        
+
         // double 매개변수 2개를 갖는 메서드 선언 및 정의
         public static double CalculateDewPoint(double F, double RH)
         {
@@ -110,7 +170,7 @@ namespace ConsoleApp1
             // 소수 둘째 자리에서 반올림하여 실수 값을 소수점 하나만 남기는 Math 라이브러리의 Round 함수 호출
             return Math.Round(CelsiusToFahrenheit(dewPoint), 1);
         }
-        
+
         // 객체의 요소를 출력
         public override String ToString()
         {
@@ -118,57 +178,109 @@ namespace ConsoleApp1
                 temperature, relativeHumidity, value);
         }
 
-        static void Main(string[] args)
+        public override void PrintTable()
         {
-            // 변수 선언
-            string[] input;
-            double RH, T;
-
-            for (;;)
+            for (int i = 0; i < _dewPointTable.GetLength(0); i++)
             {
-                // 한 줄 단위로 읽은 후 공백 단위로 각 문자열을 나누어 배열로 만듦
-                Console.Write("Enter two values: RH, T >> ");
-                input = Console.ReadLine().Split();
-
-                // 문자열 배열의 요소 개수가 2가 아니면 입력을 잘못 받은 것으로 간주하여 다시 입력받음
-                if (input.Length != 2)
+                for (int j = 0; j < _dewPointTable.GetLength(1); j++)
                 {
-                    if (input.Length == 1)
-                    {
-                        if (input[0].Equals("q") || input[0].Equals("Q"))
-                        {
-                            return; // q 또는 Q를 입력하면 프로그램 종료
-                        }
-                    }
-
-                    Console.WriteLine("Invalid Input");
-                    continue;
+                    if (!String.Equals(_dewPointTable[i, j], "-1"))
+                        Console.Write("{0}\t", _dewPointTable[i, j]);
+                    else
+                        break;
                 }
-                
-                else
-                {
-                    // 문자열의 0번째 요소는 RH 값으로, 1번째 요소는 T 값으로 변환하기 위해 double로 형 변환
-                    // 입력 문자열이 double 형으로 변환할 수 있는 문자열인지 검증하는 함수 TryParse 사용
-                    // 변환 성공 시 true를 반환하며 각 변수에 변환된 값이 저장된다.
-                    // 변환 실패 시 오류 출력
-                    if (!double.TryParse(input[0], out RH)) {
-                        Console.WriteLine("Invalid Input");
-                        continue;
-                    }
-
-                    if (!double.TryParse(input[1], out T)) {
-                        Console.WriteLine("Invalid Input");
-                        continue;
-                    }
-                    
-                    Console.WriteLine("Dew Point : {0}", CalculateDewPoint(RH, T));
-                }
+                Console.WriteLine("");
             }
         }
     }
 
     class WindChillTemperatureCalculator : Calculator
     {
+        private readonly string[,] _windChillTemperatureTable = new string[,]
+        {
+            {
+                "W/F", "40", "35", "30", "25", "20", "15", "10", "5", "0", "-5", "-10", "-15", "-20", "-25", "-30",
+                "-35", "-40", "-45"
+            },
+            {
+                "5", "36", "31", "25", "19", "13", "7", "1", "-5", "-11", "-16", "-22", "-28", "-34", "-40", "-46",
+                "-52",
+                "-57", "-63"
+            },
+            {
+                "10", "34", "27", "21", "15", "9", "3", "-4", "-10", "-16", "-22", "-28", "-35", "-41", "-47", "-53",
+                "-59", "-66", "-72"
+            },
+            {
+                "15", "32", "25", "19", "13", "6", "0", "-7", "-13", "19", "-26", "-32", "-39", "-45", "-51", "-58",
+                "-64",
+                "-71", "-77"
+            },
+            {
+                "20", "30", "24", "17", "11", "4", "-2", "-9", "-15", "-22", "-29", "-35", "-42", "-48", "-55", "-61",
+                "-68", "-74", "-81"
+            },
+            {
+                "25", "29", "23", "16", "9", "3", "-4", "-11", "-17", "-24", "-31", "-37", "-44", "-51", "-58", "-64",
+                "-71", "-78", "-84"
+            },
+            {
+                "30", "28", "22", "15", "8", "1", "-5", "-12", "-19", "-26", "-33", "-39", "-46", "-53", "-60", "-67",
+                "-73", "-80", "-87"
+            },
+            {
+                "35", "28", "21", "14", "7", "0", "-7", "-14", "-21", "-27", "-34", "-41", "-48", "-55", "-62", "-69",
+                "-76", "-82", "-89"
+            },
+            {
+                "40", "27", "20", "13", "6", "-1", "-8", "-15", "-22", "-29", "-36", "-43", "-50", "-57", "-64", "-71",
+                "-78", "-84", "-91"
+            },
+            {
+                "45", "26", "19", "12", "5", "-2", "-9", "-16", "-23", "-30", "-37", "-44", "-51", "-58", "-65", "-72",
+                "-79", "-86", "-93"
+            },
+            {
+                "50", "26", "19", "12", "4", "-3", "-10", "-17", "-24", "-31", "-38", "-45", "-52", "-60", "-67", "-74",
+                "-81", "-88", "-95"
+            },
+            {
+                "55", "25", "18", "11", "4", "-3", "-11", "-18", "-25", "-32", "-39", "-46", "-54", "-61", "-68", "-75",
+                "-82", "-89", "-97"
+            },
+            {
+                "60", "25", "17", "10", "3", "-4", "-11", "-19", "-26", "-33", "-40", "-48", "-55", "-62", "-69", "-76",
+                "-84", "-91", "-98"
+            }
+        };
+
+        public WindChillTemperatureCalculator()
+        {
+            Console.WriteLine("Calculate WindChillTemperatureCalculator");
+        }
         
+        public override void PrintTable()
+        {
+            for (int i = 0; i < _windChillTemperatureTable.GetLength(0); i++)
+            {
+                for (int j = 0; j < _windChillTemperatureTable.GetLength(1); j++)
+                {
+                    Console.Write("{0}\t", _windChillTemperatureTable[i, j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+
+    class CalculatorApp
+    {
+        static void Main(string[] args)
+        {
+            DewPointCalculator d = new DewPointCalculator();
+            d.PrintTable();
+
+            WindChillTemperatureCalculator w = new WindChillTemperatureCalculator();
+            w.PrintTable();
+        }
     }
 }
